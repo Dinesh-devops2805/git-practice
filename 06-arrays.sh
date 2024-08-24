2516e5bc6adc0563285b155917090dd757627c6b
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-24 14:09:42 +0530

    variables

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -7,6 +7,5 @@ echo "Second fruit name is: ${FRUITS[1]}"
 echo "Third fruit name is: ${FRUITS[2]}"
 
 echo "All fruits names: ${FRUITS[@]}" # here '@' symbol indicates all the iteams in a list 
-echo "All fruits names: ${FRUITS[*]}" # here '*' symbol indicates all the it
 
 
