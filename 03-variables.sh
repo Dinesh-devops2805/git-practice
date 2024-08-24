e925ef1fc89f88226a767855ecddd5372f1c281d
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-24 13:43:49 +0530

    variables

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 PERSON1=Ramesh
 PERSON2=Suresh
 
-echo "$PERSON:: Hello ${PERSON2}, how are you?"
-echo "${PERSON2}:: Hello $PERSON, I am doing good and what about you?"
-echo "$PERSON:: I am good. what's going on?"
+echo "$PERSON1:: Hello ${PERSON2}, how are you?"
+echo "${PERSON2}:: Hello $PERSON1, I am doing good and what about you?"
+echo "$PERSON1:: I am good. what's going on?"
 echo "${PERSON2}:: nothing much, just learning shell-script"
