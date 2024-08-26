d40f83006efc4bc08810cc747b5dbfc8ec8fbd0a
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-26 18:05:17 +0530

    variables

--- 12-functions.sh
+++ 12-functions.sh
@@ -21,36 +21,23 @@ fi
 
 dnf list installed git
 
-VALIDATE $? "Listing Git"
 
-# if [ $? -ne 0 ]
-# then 
-#     echo "Git is not installed. Installing it now..."
-#     dnf install git -y 
-#     if [ $? -ne 0 ]
-#     then
-#         echo "Git installation is not success.. check it once"
-#         exit 1
-#     else
-#         echo "Git is installed successfully"
-#     fi 
-# else
-#     echo "Git is already installed"
-# fi
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed. Installing it now..."
+    dnf install git -y 
+    VALIDATE $? "Installing Git"
+else
+    echo "Git is already installed"
+fi
 
-# dnf list installed mysql
+dnf list installed mysql
 
-# if [ $? -ne 0 ]
-# then 
-#     echo "MYSQL is not installed. please install it now"
-#     dnf install mysql -y 
-#     if [ $? -ne 0 ]
-#     then 
-#         echo "MYSQL installation is failure. please check it"
-#         exit 1
-#     else 
-#         echo "MYSQL is already installed and it is success"
-#     fi 
-# else 
-#     echo "MYSQL is already installed"
-# fi
+if [ $? -ne 0 ]
+then 
+    echo "MYSQL is not installed. please install it now"
+    dnf install mysql -y 
+    VALIDATE $? "Installing MySQL"
+else 
+    echo "MYSQL is already installed"
+fi
