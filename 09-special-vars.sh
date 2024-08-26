74f55b73afc721f79b96e1356cbaa08197c59af3
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-26 16:31:10 +0530

    variables

--- 09-special-vars.sh
+++ 09-special-vars.sh
@@ -5,3 +5,7 @@ echo "Number of variables passed: $#"
 echo "To know the script name: $0"
 echo "To know the current working directory: $PWD"
 echo "To know the home directory of current user: $HOME"
+echo "Process instance ID of the script executing now: $$"
+sleep 100 & 
+echo "PID of last backgroun command: $!"
+
