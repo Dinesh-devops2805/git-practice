3b579294db8e696bc9815a380b96abc36ef66bf0
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-26 16:26:00 +0530

    variables

---
+++ 09-special-vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "All variables passed to the script: $@"
+echo "Number of variables passed: $#"
+echo "To know the script name: $0"
+echo "To know the current working directory: $PWD"
+echo "To know the home directory of current user: $HOME"
