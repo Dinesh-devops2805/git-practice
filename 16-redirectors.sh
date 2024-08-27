6b1df42dcdc5f298062fdecb569524ea2bba884b
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-27 13:49:16 +0530

    redirectors

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -11,6 +11,7 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
     if [ $USERID -ne 0 ]
@@ -29,4 +30,26 @@ VALIDATE(){
     fi
 }
 
+USAGE(){
+    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2...."
+}
+
 CHECK_ROOT
+
+if [ $# -eq 0]
+then
+    USAGE
+fi
+
+for package in $@  # $@ refers to all arguments passed to it
+do 
+    dnf list installed $package &>>$LOG_FILE
+    if [ $? -ne 0 ]
+    then
+        echo " $package is not instaleld, going to install it now.." &>>$LOG_FILE
+        dnf install $package -y  &>>$LOG_FILE
+        VALIDATE $? "Installing $package"
+    else 
+        echo -e "$package is already $Y installed.. nothing to do $N" &>>$LOG_FILE
+    fi
+done 
