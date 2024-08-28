5496288de73f09fe7b68345a9a2ea1243a91efbc
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-28 17:52:38 +0530

    mysql-shell

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SOURCE_DIR=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$SOURCE_DIR $G exists $N"
+else 
+    echo -e "$SOURCE_DIR $R does not exist $N"
+    exit 1 
+fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files: $FILES"
+
+while IFS= read -r line    #IFS, internal field seperator, empty it will ignore while space. -r is for not to ignore special characters like /
+do 
+    echo "Deleting lines: $line"
+    rm -rf $line
+done <<< $FILES 
