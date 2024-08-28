19708230644232d5042f4b7b0e1535cad47c39a9
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-28 16:40:35 +0530

    mysql-shell

--- 17-set.sh
+++ 17-set.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -ex   # setting the automatic exit, if we get error, set -ex for debug
+
+failure(){
+    echo "Error at: $1:$2"
+}
+
+trap 'failure "${LINENO}" "{BASH_COMMAND}"' ERR  # ERR is an error signal
+
 echo "Hello world"
 echoooooo "Hello world"
 echo "Hello world after failure"
