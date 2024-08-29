ca5b810902facb567b6adfd3ddbbc23800ef647e
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-29 15:30:14 +0530

    backup file

--- 19-backup.sh
+++ 19-backup.sh
@@ -32,31 +32,30 @@ then
     echo -e "$R Source directory $DEST_DIR does not exist.. please check $N"
 fi 
 
-FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime $DAYS)
+FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 
 echo "Files: $FILES"
 
-if [ ! -z $FILES ]    # true if FILES is empty, ! makes it expression false
+if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
 then
     echo "Files are found"
     ZIP_FILE="$DEST_DIR/app-logs-$TIMESTAMP.zip"
-    find ${SOURCE_DIR} -name "*.log" -mtime $DAYS | zip "$ZIP_FILE" -@
+    find ${SOURCE_DIR} -name "*.log" -mtime +14 | zip "$ZIP_FILE" -@
 
-    #check if zip file is successfully craeted or not
+    #check if zip file is successfully created or not
     if [ -f $ZIP_FILE ]
     then
-        echo -e "$G Zip file created successfully older than $DAYS"
-    
-        # remopve the files after zipping
-        while IFS= read -r file    #IFS, internal field seperator, empty it will ignore while space. -r is for not to ignore special characters like /
-        do 
-            echo "Deleting files: $file"
-            m -rf $file
-        done <<< $FILES 
+        echo "Successfully zippped files older than $DAYS"
+        #remove the files after zipping
+        while IFS= read -r file #IFS,internal field seperatpor, empty it will ignore while space.-r is for not to ingore special charecters like /
+        do
+            echo "Deleting file: $file"
+            rm -rf $file
+        done <<< $FILES
     else
         echo "Zipping the files is failed"
-        exit 1 
+        exit 1
     fi
 else
-    echo "No files found older than $DAYS"
+    echo "No files older than $DAYS"
 fi
