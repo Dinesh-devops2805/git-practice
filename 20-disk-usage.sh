0f2c58477c7b5b1df4db9871e24fa5e57a5ba21c
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-29 16:02:34 +0530

    disk storage

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -3,6 +3,7 @@
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5   #real projects, it is usually 75
 
+
 while IFS= read -r line    #IFS, internal field seperator, empty it will ignore while space. -r is for not to ignore special characters like /
 do 
     echo $line 
