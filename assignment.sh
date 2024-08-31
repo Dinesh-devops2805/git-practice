edb49793cb964509db83e353eb80aba503d622b9
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-31 16:21:46 +0530

    assignment

--- assignment.sh
+++ assignment.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-# algorithm 
-# 1. Directory  -- mkdir devops 
-# 2. multiple files linux.txt and shell-script.txt in it
-# 3. find and replace the specific string
-
-#You need to find and replace a specific string in multiple text files in a directory in shell script
-
-
-
-DIR=$1    # 
-STRING=$2   # arguement
+# Set the directory, old string, and new string from command-line arguments
+DIR=$1
+OLD_STRING=$2
 NEW_STRING=$3
-FILES=($(find "$DIR" -type f -name "*.txt"))
-for file in "${FILES[@]}"
-do
-sed -i "s/$STRING/$NEW_STRING/g" "$file"
-done
 
+# Print usage message if no arguments are provided
+if [ -z "$DIR" ] || [ -z "$OLD_STRING" ] || [ -z "$NEW_STRING" ]
+then
+  echo "Usage: $0 <directory> <old_string> <new_string>"
+  exit 1
+fi
+
+# Find all .txt files in the specified directory and its subdirectories
+FILES=($(find "$DIR" -type f -name "*.txt"))
+
+# Loop through each file and replace the old string with the new string
+for file in "${FILES[@]}"; do
+  sed -i "s/$OLD_STRING/$NEW_STRING/g" "$file"
+  echo "Replaced '$OLD_STRING' with '$NEW_STRING' in $file"
+done
 
 
 
