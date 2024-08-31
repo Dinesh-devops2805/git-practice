910f573b299ce45e562e884fddd728afe628e1a9
lakshmimungara <lakshmimungara2997@gmail.com>
2024-08-31 16:58:06 +0530

    assignment

--- assignment.sh
+++ assignment.sh
@@ -7,22 +7,22 @@ NEW_STRING=$3
 
 
 # Print usage message if no arguments are provided
-if [ -z "$DIR" ] || [ -z "$OLD_STRING" ] || [ -z "$NEW_STRING" ]
-then
-  echo "Usage: $0 <directory> <old_string> <new_string>"
-  exit 1
-fi
+# if [ -z "$DIR" ] || [ -z "$OLD_STRING" ] || [ -z "$NEW_STRING" ]
+# then
+#   echo "Usage: $0 <directory> <old_string> <new_string>"
+#   exit 1
+# fi
 
-# Find all .txt files in the specified directory and its subdirectories
-FILES=($(find "$DIR" -type f -name "*.txt"))
+# # Find all .txt files in the specified directory and its subdirectories
+# FILES=($(find "$DIR" -type f -name "*.txt"))
 
-# Loop through each file and replace the old string with the new string
-for file in "${FILES[@]}"; do
-  sed -i "s/$OLD_STRING/$NEW_STRING/g" "$file"
-  echo "Replaced '$OLD_STRING' with '$NEW_STRING' in $file"
-done
+# # Loop through each file and replace the old string with the new string
+# for file in "${FILES[@]}"; do
+#   sed -i "s/$OLD_STRING/$NEW_STRING/g" "$file"
+#   echo "Replaced '$OLD_STRING' with '$NEW_STRING' in $file"
+# done
 
-mkdir devops
+dir devops
 if [ $DIR -ne 0 ]
 then
 echo "Directory does not exist"
